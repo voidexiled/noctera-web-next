a813bf2143ed1fec287f7ef4664c61bbb497c311
voidexiled <l20070518@cdmadero.tecnm.mx>
2024-12-05 13:57:54 -0600

    feat: order by quantity in webshop

--- src/app/(webshop)/shop/page.tsx
+++ src/app/(webshop)/shop/page.tsx
@@ -164,7 +164,7 @@ export default function PremiumHistory() {
 
                       <RHFRadioGroupItemShop
                         name="product"
-                        options={products}
+                        options={products.toSorted(((a, b) => a.quantity! - b.quantity!))}
                         defaultValue={watch('product')}
                       />
 
