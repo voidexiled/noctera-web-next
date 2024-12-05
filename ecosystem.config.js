60a9a590f05e37547588cfbc4187ae12d6560f62
root <root@noctera-global-2025.us-central1-c.c.dev-server-439404.internal>
2024-12-05 11:45:28 +0000

    updated from vps

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,17 +4,16 @@ module.exports = {
   }],
 
   deploy : {
-    production : {
-	key: 'key.pem',
-      user : 'noctera_global',
-      host : '34.16.96.148',
+    production: {
+      user : 'noctera_user_new',
+      host : '127.0.0.1',
       ref  : 'origin/main',
-      repo : 'git@github.com:voidexiled/noctera-web-next.git',
+      repo : 'https://github.com/voidexiled/noctera-web-next.git',
       path : '/home/noctera_user_new',
       'pre-deploy-local': '',
       'post-deploy' : 'pnpm install && pnpm build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
-	'ssh_options': 'ForwardAgent=yes'
+
     }
   }
 };
